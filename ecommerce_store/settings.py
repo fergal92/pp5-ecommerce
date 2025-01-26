7fa2382f7205425d702faeabe5af69557d564e0c
Fergal Mulligan <fshaq4@gmail.com>
2025-01-26 22:03:24 +0000

    Add live postgres database and sync with heroku

--- ecommerce_store/settings.py
+++ ecommerce_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 if os.path.exists('env.py'):
     import env
@@ -28,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-fergal92-pp5ecommerce-ff0vcg2m6qt.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['.herokuapp.com','8000-fergal92-pp5ecommerce-ff0vcg2m6qt.ws.codeinstitute-ide.net']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-fergal92-pp5ecommerce-ff0vcg2m6qt.ws.codeinstitute-ide.net'
@@ -126,12 +127,17 @@ WSGI_APPLICATION = 'ecommerce_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
